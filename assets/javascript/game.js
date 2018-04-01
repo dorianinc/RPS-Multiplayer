3a42f94c7cfc835ff4de11da45e9b24e421b4602
Dorian Macias <33275910+dorianinc@users.noreply.github.com>
2018-04-01 11:58:01 -0700

    Add files via upload
    - Added boxes that will contain players choices and round results

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// Global Variables //
+/*-------------------------------------------------------------------*/
+var time = 30;
+var timer;
+
+var correct = 0;
+var inCorrect = 0;
+var unAnswered = 0;
+
+var currentQuestion;
+var currentQuestionNumber = 0;
